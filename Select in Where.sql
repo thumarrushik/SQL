ded53e7df42c9603736e6da2621f5421f828b2ea
Rushik Thumar <rpt@gwu.edu>
2017-12-04 19:00:26 -0500

    Create Select in Where.sql

---
+++ Select in Where.sql
@@ -0,0 +1,7 @@
+SELECT ROUND(LONG_W,4)
+FROM STATION
+WHERE LAT_N = (SELECT MAX(LAT_N) FROM STATION WHERE LAT_N < 137.2345);
+
+/*
+Query the Western Longitude (LONG_W) for the largest Northern Latitude (LAT_N) in STATION that is less than . Round your answer to  decimal places.
+*/
